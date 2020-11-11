1680c019fb991b099dbac7c7c14db5ae7ab9a500
Kira <kors9442@gmail.com>
2020-11-11 20:26:13 +0300

    practice 2

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
-const numberOfFilms = +prompt("How many movies have you watched?", '');
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt("How many movies have you watched?", '');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("How many movies have you watched?", '');
+    }
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,30 +15,47 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-
-
-if (personalMovieDB.count < 10){
-    alert('Not many movies');
-}
-else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    alert('I see you like movies');
-}
-else if(personalMovieDB.count >= 30){
-    alert('You really into it');
-}
-else{
-    alert('Something went wrong');
+function showMyDB(status){
+    if (!status)
+        console.log(personalMovieDB);
 }
 
-for (let i = 0; i < personalMovieDB.count; i++){
-    const lastMovie = prompt('What was the last movie you have whatched?', '');
-    const lastMovieRate = prompt('How you rate it?', '');
-    if (lastMovie != null && lastMovieRate != null && lastMovie != '' && lastMovieRate != '' && lastMovie.length < 50){
-        personalMovieDB.movies[lastMovie] = lastMovieRate;
+function writeYourGenres(){
+    for (let i = 0; i < 3; i++){
+        personalMovieDB.genres[i] = prompt(`Your favorite genre number ${i+1}`);
+    }
+    console.log(personalMovieDB);
+}
+writeYourGenres();
+
+function rememberMovies(){
+    for (let i = 0; i < 2; i++){
+        const lastMovie = prompt('What was the last movie you have whatched?', '');
+        const lastMovieRate = prompt('How you rate it?', '');
+        if (lastMovie != null && lastMovieRate != null && lastMovie != '' && lastMovieRate != '' && lastMovie.length < 50){
+            personalMovieDB.movies[lastMovie] = lastMovieRate;
+        }
+        else{
+            i--;
+        }
+    }
+}
+
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10){
+        alert('Not many movies');
+    }
+    else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        alert('I see you like movies');
+    }
+    else if(personalMovieDB.count >= 30){
+        alert('You really into it');
     }
     else{
-        i--;
+        alert('Something went wrong');
     }
 }
+
 
 console.log(personalMovieDB);
